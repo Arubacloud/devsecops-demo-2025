f47a0587371feffc127e8f524907a0ffc3651e68
aru-amedeo <amedeo.palopoli@staff.aruba.it>
2025-11-01 08:34:30 +0100

    fix cloudserver docker build

--- build/cloudserver/internal/db/setup.go
+++ build/cloudserver/internal/db/setup.go
@@ -1,42 +1,41 @@
 package db
-package db
 
 import (
-    "database/sql"
-    "fmt"
-    "os"
-    "time"
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
 
-    _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func InitDB() *sql.DB {
-    user := os.Getenv("DB_USERNAME")
-    pass := os.Getenv("DB_PASSWORD")
-    host := os.Getenv("DB_HOST")
-    name := os.Getenv("DB_NAME")
-    dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?parseTime=true", user, pass, host, name)
+	user := os.Getenv("DB_USERNAME")
+	pass := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	name := os.Getenv("DB_NAME")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?parseTime=true", user, pass, host, name)
 
-    var db *sql.DB
-    var err error
-    for i := 0; i < 10; i++ {
-        db, err = sql.Open("mysql", dsn)
-        if err == nil && db.Ping() == nil {
-            break
-        }
-        time.Sleep(3 * time.Second)
-    }
-    if err != nil {
-        panic(fmt.Sprintf("Failed to connect to DB: %v", err))
-    }
+	var db *sql.DB
+	var err error
+	for i := 0; i < 10; i++ {
+		db, err = sql.Open("mysql", dsn)
+		if err == nil && db.Ping() == nil {
+			break
+		}
+		time.Sleep(3 * time.Second)
+	}
+	if err != nil {
+		panic(fmt.Sprintf("Failed to connect to DB: %v", err))
+	}
 
-    _, err = db.Exec(`CREATE TABLE IF NOT EXISTS items (
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS items (
         id INT AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         description TEXT
     )`)
-    if err != nil {
-        panic(fmt.Sprintf("Error creating table: %v", err))
-    }
-    return db
+	if err != nil {
+		panic(fmt.Sprintf("Error creating table: %v", err))
+	}
+	return db
 }
